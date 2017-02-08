ab7bb2da671f04346f8bd47be954f16de50972a9
David Ells <ells.david@gmail.com>
2017-02-08 14:58:34 -0600

    Add git revision to dist package

--- tasks/package-aws.sh
+++ tasks/package-aws.sh
@@ -5,5 +5,9 @@ export NODE_ENV=production
 export PATH=$PATH:`npm bin`
 
 ./tasks/dist.sh
+
+GITREV=`git rev-parse HEAD`
+GITREV=${GITREV:0:8}
+
 cd dist 
-zip -r ../dist.zip --exclude='*node_modules*' .
+zip -r ../dist-${GITREV}.zip --exclude='*node_modules*' .
